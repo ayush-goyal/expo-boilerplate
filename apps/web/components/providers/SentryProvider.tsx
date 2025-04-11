5e7bad72602fb0cb787617da0cdbc15ff90bfdec
Ayush Goyal <agoyal2001@gmail.com>
2025-04-11 11:17:55 -0400

    Fix issues for sentry provider on frontend

--- apps/web/components/providers/SentryProvider.tsx
+++ apps/web/components/providers/SentryProvider.tsx
@@ -1,28 +1,11 @@
 "use client";
 
-import { useEffect } from "react";
-import * as Sentry from "@sentry/nextjs";
-
-import { authClient } from "@/lib/auth-client";
-
 interface SentryProviderProps {
   children: React.ReactNode;
 }
 
 export function SentryProvider({ children }: SentryProviderProps) {
-  const { data: session } = authClient.useSession();
-
-  // Set user context when session changes
-  useEffect(() => {
-    if (session?.user) {
-      Sentry.setUser({
-        id: session.user.id,
-        email: session.user.email,
-      });
-    } else {
-      Sentry.setUser(null);
-    }
-  }, [session]);
+  // TODO: Setup firebase and identify user
 
   return children;
 }
