8c7466c42ae6ced76e6fa7c4eb94d9978e03fcf1
Ayush Goyal <agoyal2001@gmail.com>
2025-05-01 21:50:29 -0400

    Set google cloud env vars for production deployment

--- packages/api/src/firebase.ts
+++ packages/api/src/firebase.ts
@@ -1,8 +1,14 @@
-import { applicationDefault, getApps, initializeApp } from "firebase-admin/app";
+import { applicationDefault, cert, getApps, initializeApp } from "firebase-admin/app";
 
 export const app =
   getApps()[0] ||
   initializeApp({
-    // Initialized via process.env.GOOGLE_APPLICATION_CREDENTIALS
-    credential: applicationDefault(),
+    credential:
+      process.env.NODE_ENV === "development"
+        ? applicationDefault()
+        : cert({
+            projectId: process.env.GOOGLE_CLOUD_PROJECT,
+            privateKey: process.env.GOOGLE_CLOUD_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+            clientEmail: process.env.GOOGLE_CLOUD_CLIENT_EMAIL,
+          }),
   });
