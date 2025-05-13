8c0ee324960c65e69d4d4c38d370800ae3d1c83f
Ayush Goyal <agoyal2001@gmail.com>
2025-05-13 09:23:14 -0400

    Fix zustand import issue in release

--- apps/native/babel.config.js
+++ apps/native/babel.config.js
@@ -2,7 +2,17 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: [["babel-preset-expo", { jsxImportSource: "nativewind" }], "nativewind/babel"],
+    presets: [
+      [
+        "babel-preset-expo",
+        {
+          jsxImportSource: "nativewind",
+          // https://github.com/pmndrs/zustand/discussions/1967#discussioncomment-12801707
+          unstable_transformImportMeta: true,
+        },
+      ],
+      "nativewind/babel",
+    ],
     plugins: ["react-native-reanimated/plugin"],
   };
 };
