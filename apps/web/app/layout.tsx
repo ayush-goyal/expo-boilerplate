71efadfdefbe34325f95703c5868e589d20a155f
Ayush Goyal <agoyal2001@gmail.com>
2025-08-24 10:51:18 -0400

    Update app metadata

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -10,8 +10,28 @@ import "@/styles/globals.css";
 import { Toaster } from "@/components/ui/sonner";
 
 export const metadata: Metadata = {
-  title: "Next App",
-  icons: [{ rel: "icon", url: "/favicon.ico" }],
+  title: "Acme",
+  description: "Acme is a platform for managing your business.",
+  keywords: ["acme", "platform", "business", "management"],
+  icons: {
+    icon: [
+      { url: "/favicon.ico" },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      {
+        url: "/android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        url: "/android-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+    shortcut: "/favicon.ico",
+    apple: "/apple-touch-icon.png",
+  },
 };
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
