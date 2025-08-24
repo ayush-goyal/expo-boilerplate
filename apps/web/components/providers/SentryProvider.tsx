2cfb4ad3f4b7325e1ff0c3755d416aa0a5ddfc7b
Ayush Goyal <agoyal2001@gmail.com>
2025-08-24 10:51:08 -0400

    Update sentry provider

--- apps/web/components/providers/SentryProvider.tsx
+++ apps/web/components/providers/SentryProvider.tsx
@@ -1,11 +1,28 @@
 "use client";
 
+import { useEffect } from "react";
+import * as Sentry from "@sentry/nextjs";
+
+import { authClient } from "@/lib/auth-client";
+
 interface SentryProviderProps {
   children: React.ReactNode;
 }
 
 export function SentryProvider({ children }: SentryProviderProps) {
-  // TODO: Setup firebase and identify user
+  const { data: session } = authClient.useSession();
+
+  // Set user context when session changes
+  useEffect(() => {
+    if (session?.user) {
+      Sentry.setUser({
+        id: session.user.id,
+        email: session.user.email,
+      });
+    } else {
+      Sentry.setUser(null);
+    }
+  }, [session?.user]);
 
   return children;
 }
